812a4bcf959a9d151baf3d6d77c1200c87ebaece
PortSol <web.portsol@gmail.com>
2024-04-07 17:40:44 +0530

    Add about page to polls

--- polls/views.py
+++ polls/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World!!, Welcome to Polls app")
+
+def about(request):
+    return HttpResponse("We are Polls, folks!!")
