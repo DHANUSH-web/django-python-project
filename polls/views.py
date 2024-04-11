170551a5b6a21cf4e0c105f1325f05cf43093be4
PortSol <web.portsol@gmail.com>
2024-04-11 23:45:28 +0530

    Delete polls django app

--- polls/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Hello World!!, Welcome to Polls app")
-
-def about(request):
-    return HttpResponse("We are Polls, folks!!")
