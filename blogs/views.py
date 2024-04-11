652275386226064f68adff6a5f56925c91370ae2
PortSol <web.portsol@gmail.com>
2024-04-11 23:56:24 +0530

    Add about page template and title parameter

--- blogs/views.py
+++ blogs/views.py
@@ -2,7 +2,7 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request, "index.html", dict(page="home"))
+    return render(request, "index.html", { "title": "Django - Home" })
 
 def about(request: HttpRequest) -> HttpResponse:
-    return render(request, "index.html", dict(page="about"))
+    return render(request, "about.html", { "title": "Django - About" })
