887cd26dfef9356147b8e70ebedf3e5310f92afe
PortSol <web.portsol@gmail.com>
2024-04-11 23:59:39 +0530

    Indicate arguments for better code understanding

--- blogs/views.py
+++ blogs/views.py
@@ -2,7 +2,7 @@ from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 def home(request: HttpRequest) -> HttpResponse:
-    return render(request, "index.html", { "title": "Django - Home" })
+    return render(request=request, template_name="index.html", context={ "title": "Django - Home" })
 
 def about(request: HttpRequest) -> HttpResponse:
-    return render(request, "about.html", { "title": "Django - About" })
+    return render(request=request, template_name="about.html", context={ "title": "Django - About" })
